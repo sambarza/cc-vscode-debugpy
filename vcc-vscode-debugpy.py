0b52cb58a83fa19be29a336b84aba55f23e2006e
Samuele Barzaghi <sambarza@gmail.com>
2023-08-23 00:55:41 +0200

    Initial commit

---
+++ vcc-vscode-debugpy.py
@@ -0,0 +1,11 @@
+from cat.mad_hatter.decorators import tool, hook
+
+import debugpy
+
+@tool(return_direct=True)
+def activate_the_debugger(tool_input, cat):
+    """Replies to "activate the debugger", "prepare for debug session", "I want to debug" or similar questions. Input is always """
+
+    debugpy.listen(("0.0.0.0", 5678))
+
+    return "I'm ready, try to connect with VSCode now"
