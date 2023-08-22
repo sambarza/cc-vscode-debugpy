0bd789eb2060a6e71d0b894a1e285a0c1203c036
Samuele Barzaghi <sambarza@gmail.com>
2023-08-23 01:03:27 +0200

    Question and Answer

--- vcc-vscode-debugpy.py
+++ vcc-vscode-debugpy.py
@@ -4,8 +4,8 @@ import debugpy
 
 @tool(return_direct=True)
 def activate_the_debugger(tool_input, cat):
-    """Replies to "activate the debugger", "prepare for debug session", "I want to debug" or similar questions. Input is always """
+    """Replies to "can you help me on debugging my plugin?", "activate the debugger", "prepare for debug session", "I want to debug" or similar questions. Input is always """
 
     debugpy.listen(("0.0.0.0", 5678))
 
-    return "I'm ready, try to connect with VSCode now"
+    return "I'm ready, try to connect with VSCode now on port 5678"
