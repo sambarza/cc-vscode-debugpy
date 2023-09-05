0154e6ad931cb2b5076be4d6c2ba96426b9fdfe7
Samuele Barzaghi <sambarza@gmail.com>
2023-09-05 21:50:05 +0200

    Use cat.mad_hatter.plugins.load_settings

--- cc-vscode-debugpy.py
+++ cc-vscode-debugpy.py
@@ -1,14 +1,16 @@
 from cat.mad_hatter.decorators import tool, hook
 
-import os
-import json
-from pydantic import BaseModel
 from cat.log import log
+from pydantic import BaseModel
+
 
 import debugpy
 
 # This is the fixed port exposed in docker-compose.yml
 LISTENING_PORT = 5678
+
+# Plugin name, used to load settings
+PLUGIN_NAME = "cc-vscode-debugpy"
 
 
 class MySettings(BaseModel):
@@ -22,7 +24,7 @@ def plugin_settings_schema():
 
 @hook
 def before_cat_bootstrap(cat):
-    settings = load_settings()
+    settings = cat.mad_hatter.plugins[PLUGIN_NAME].load_settings()
 
     if "listen_on_bootstrap" in settings and settings["listen_on_bootstrap"]:
         try:
@@ -53,36 +55,10 @@ def start_listening():
 
 
 def wait_for_client():
-    """Wait for a client connection, this blocking"""
+    """Wait for a client connection, this is blocking"""
 
     log(
         f"Waiting for debug sessions on port {LISTENING_PORT}, attach with VSCode to continue startup",
         "WARNING",
     )
     debugpy.wait_for_client()
-
-
-def load_settings():
-    """This is copy and paste of the core method `Plugin.load_settings()`
-    Investigate how to reuse the core method...
-    """
-
-    path = "cat/plugins/cc-vscode-debugpy/"
-
-    # by default, plugin settings are saved inside the plugin folder
-    #   in a JSON file called settings.json
-    settings_file_path = os.path.join(path, "settings.json")
-
-    # default settings is an empty dictionary
-    settings = {}
-
-    # load settings.json if exists
-    if os.path.isfile(settings_file_path):
-        try:
-            with open(settings_file_path, "r") as json_file:
-                settings = json.load(json_file)
-        except Exception as e:
-            print(f"Unable to load plugin cc-vscode-debugpy settings", "ERROR")
-            print(e, "ERROR")
-
-    return settings
